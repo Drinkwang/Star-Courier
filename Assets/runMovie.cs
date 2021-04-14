4f23c7c3dabf2ac376cbd2493e8a9604b7b3c43e
wzp <15271657400@163.com>
2021-04-15 07:52:57 +0800

    vedio maker

---
+++ Assets/runMovie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+[ExecuteInEditMode]
+public class runMovie : MonoBehaviour
+{ 
+    public bool isPlayMovie ;
+    public int frame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VideoPlayer tempVideo= this.GetComponent<VideoPlayer>();
+        tempVideo.Play();
+        if (isPlayMovie == true)
+        {
+
+
+
+        }
+        else
+        {
+
+            if (tempVideo.frame != (int)frame)
+            {
+                tempVideo.frame = (int)frame;
+                Debug.Log("is ruing");
+            }
+
+
+        }
+
+
+    }
+}
